ec8962f1dd25cd331e44116a65b95b3e8ce00a94
mfg637 <mfg636@gmail.com>
2022-08-06 20:26:57 +0300

    first working implementation that can only webp transcode

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import pathlib
-
+import pyimglib
+import argparse
 
 if __name__ == '__main__':
-    pass
+    argument_parser = argparse.ArgumentParser()
+    #argument_parser.add_argument("-i", type=pathlib.Path)
+    argument_parser.add_argument("source-images", type=pathlib.Path, nargs="+")
+    argument_parser.add_argument("output-directory", type=pathlib.Path)
+    args = argument_parser.parse_args()
+    outdir: pathlib.Path = getattr(args, 'output-directory')
+    image_files: list[pathlib.Path] = getattr(args, 'source-images')
+    for image_file in image_files:
+        transcoder = pyimglib.transcoding.get_file_transcoder(str(image_file), str(outdir), image_file.stem, {}, {})
+        print(image_file, transcoder)
+        transcoder.transcode()
+
+
