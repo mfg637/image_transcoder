8824ec00636d38ed76b847c238812f39db3dc62d
mfg637 <mfg636@gmail.com>
2022-08-08 23:54:10 +0300

    show transcoding statistics

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import pathlib
 import pyimglib
 import argparse
 import config
+import logging
+
+logging.basicConfig(
+    format="%(process)dx%(thread)d::%(levelname)s::%(name)s::%(message)s", level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     argument_parser = argparse.ArgumentParser()
-    #argument_parser.add_argument("-i", type=pathlib.Path)
     argument_parser.add_argument("source-images", type=pathlib.Path, nargs="+")
     argument_parser.add_argument("output-directory", type=pathlib.Path)
     args = argument_parser.parse_args()
     outdir: pathlib.Path = getattr(args, 'output-directory')
     image_files: list[pathlib.Path] = getattr(args, 'source-images')
     for image_file in image_files:
-        transcoder = pyimglib.transcoding.get_file_transcoder(str(image_file), outdir, image_file.stem, {}, {})
-        print(image_file, transcoder)
-        transcoder.transcode()
+        transcoder = pyimglib.transcoding.get_file_transcoder(str(image_file), outdir, image_file.stem)
+        result = transcoder.transcode()
+        pyimglib.transcoding.statistics.update_stats([result])
+    pyimglib.transcoding.statistics.log_stats()
 
 
