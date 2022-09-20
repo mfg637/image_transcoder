cee93d6e0338bf754d0a32a5305e13688faa1b53
mfg637 <mfg636@gmail.com>
2022-09-20 15:19:49 +0300

    allow to set aomenc cpu-used option

--- main.py
+++ main.py
@@ -15,12 +15,15 @@ if __name__ == '__main__':
     argument_parser.add_argument("source-images", type=pathlib.Path, nargs="+")
     argument_parser.add_argument("output-directory", type=pathlib.Path)
     argument_parser.add_argument("--force-audio-transcode", action="store_true")
+    argument_parser.add_argument("--cpu-used", type=int, default=None)
     args = argument_parser.parse_args()
     outdir: pathlib.Path = getattr(args, 'output-directory')
     image_files: list[pathlib.Path] = getattr(args, 'source-images')
+    if args.cpu_used is not None:
+        pyimglib.config.aomenc_cpu_usage = args.cpu_used
     pyimglib.config.force_audio_transcode = args.force_audio_transcode
     for image_file in image_files:
-        transcoder = pyimglib.transcoding.get_file_transcoder(str(image_file), outdir, image_file.stem)
+        transcoder = pyimglib.transcoding.get_file_transcoder(image_file, outdir, image_file.stem)
         result = transcoder.transcode()
         pyimglib.transcoding.statistics.update_stats([result])
     pyimglib.transcoding.statistics.log_stats()
