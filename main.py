36cb429f91eeab88152d20e4d902adcfe996573b
mfg637 <mfg636@gmail.com>
2022-09-20 14:25:35 +0300

    allow to force audio transcoding

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ if __name__ == '__main__':
     argument_parser = argparse.ArgumentParser()
     argument_parser.add_argument("source-images", type=pathlib.Path, nargs="+")
     argument_parser.add_argument("output-directory", type=pathlib.Path)
+    argument_parser.add_argument("--force-audio-transcode", action="store_true")
     args = argument_parser.parse_args()
     outdir: pathlib.Path = getattr(args, 'output-directory')
     image_files: list[pathlib.Path] = getattr(args, 'source-images')
+    pyimglib.config.force_audio_transcode = args.force_audio_transcode
     for image_file in image_files:
         transcoder = pyimglib.transcoding.get_file_transcoder(str(image_file), outdir, image_file.stem)
         result = transcoder.transcode()
